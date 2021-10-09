5dd9812b2d0f8f4fc7aa30963ad6e2cf2b96d193
Shivppatel <shiv.p.patel@outlook.com>
2021-10-09 19:09:00 -0400

    updated database connection  host

--- server/database/config.js
+++ server/database/config.js
@@ -3,21 +3,21 @@ export default {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '54.196.33.144',
+    host: '34.201.154.38',
     dialect: 'mysql'
   },
   test: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '54.196.33.144',
+    host: '34.201.154.38',
     dialect: 'mysql'
   },
   production: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '54.196.33.144',
+    host: '34.201.154.38',
     dialect: 'mysql'
   }
 };
