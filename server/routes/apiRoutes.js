0938cbbe272dd4167b1d4f47e1f33fd5134305b9
Alex Leitch <aleitch1@umd.edu>
2021-12-02 11:33:06 -0500

    Add new filtered SQL request

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -59,21 +59,36 @@ router.route('/foodServicesPG')
     }
   });
 
-const mealMapCustom = `SELECT hall_name,
-  hall_address,
-  hall_lat,
-  hall_long,
-  meal_name
+// const mealMapCustom = `SELECT hall_name,
+//   hall_address,
+//   hall_lat,
+//   hall_long,
+//   meal_name
+// FROM
+//   Meals m
+// INNER JOIN Meals_Locations ml
+//   ON m.meal_id = ml.meal_id
+// INNER JOIN Dining_Hall d
+// ON d.hall_id = ml.hall_id;`;
+
+const hallIdQuery = `SELECT hall_name,
+hall_address,
+hall_lat,
+hall_long,
+meal_name 
 FROM
-  Meals m
+Meals m
 INNER JOIN Meals_Locations ml 
   ON m.meal_id = ml.meal_id
 INNER JOIN Dining_Hall d
-ON d.hall_id = ml.hall_id;`;
+ON d.hall_id = ml.hall_id
+WHERE ml.hall_id = :hall_id;`;
+
 router.route('/sqlDemo')
   .post(async (req, res) => {
     try {
-      const result = await db.sequelizeDB.query(mealMapCustom, {
+      const result = await db.sequelizeDB.query(hallIdQuery, {
+        replacements: { hall_id: '1' },
         type: sequelize.QueryTypes.SELECT
       });
       res.json(result);
