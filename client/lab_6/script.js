6578d4d56ca9a405f247b6a8f2bf80fe742fd072
Alex Leitch <aleitch1@umd.edu>
2022-01-13 17:35:50 -0500

    Add notes to Lab 6 as to where to get your code

---
+++ client/lab_6/script.js
@@ -0,0 +1,9 @@
+// copy the annotated script from Lab 5 in here
+// then add a new function called 'dataHandler'
+// Taking one argument, an array
+// and call it within your 'submit' event listener
+/*
+    looks a bit like this:
+    function dataHandler(array) {}
+    etc.
+*/
