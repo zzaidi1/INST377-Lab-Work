01e5bffe1b74230de8d5836e4f23032a4838fda6
Alex Leitch <aleitch1@umd.edu>
2022-07-21 11:55:05 -0400

    Reduce to SQL-only ORM

--- server/database/initializeDB.js
+++ server/database/initializeDB.js
@@ -18,36 +18,6 @@ if (config.use_env_variable) {
 
 const db = {};
 
-// If you are choosing to use this database with models
-// This is where the models attach themselves to your database
-// It is commented out because ORM management is a lot for a basic class.
-
-/* Associate models to database and return resulting object */
-// const db = Object.keys(modelList).reduce((collection, modelName) => {
-//   if (!collection[modelName]) {
-//     // eslint-disable-next-line no-param-reassign
-//     collection[modelName] = modelList[modelName](sequelizeDB, DataTypes);
-//   }
-//   return collection;
-// }, {});
-
-/* Fire off associations to make sure tables can call one another through models */
-// Object.keys(db).forEach((modelName) => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-/* Resulting object shape */
-/* {
-    {
-      Meals: // a sequelize entry that connects the model for Meals
-      Halls: // Halls
-      Macros: // Macros
-    }
-  }
-*/
-
 db.sequelizeDB = sequelizeDB;
 db.Sequelize = Sequelize;
 
