ee645de052d9173e0d0f72d301ec54feb4c32f20
Alex Leitch <aleitch1@umd.edu>
2022-07-23 09:38:27 -0400

    Annotate apiRoutes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,15 +1,36 @@
 /* eslint-disable no-console */
 import express from 'express';
-import fetch from 'node-fetch'; // We use node-fetch because it mimics a browser fetch request
 
 import foodServiceRoutes from './foodServiceRoutes.js';
 import sqlDemoRoutes from './sqlDemoRoutes.js'; // this is included in case you want to use a database of your own for the group project
 
 const router = express.Router();
 
+/* eslint-disable max-len */
+/*
+  ## What is this file?
+  This is the main "router" for the server-side of your application.
+  It listens for address requests - like 'http://your-app.heroku.com/api' - and sends a response.
+  The response can be anything.
+
+  In this file we have three demonstrations, an HTML page, a basic external API request, and a SQL request.
+
+  For a long time, when we did a "get" request - asked for an address - JUST an HTML page would come back.
+  Now, though, most clients are built of Javascript, which changes our route structure a lot.
+
+  In this class, we separate our data routes and our content routes.
+
+  Content is available at 'http://your-app.heroku.com/'
+  Data is available at 'http://your-app.heroku.com/api' - you can find this command at line
+
+*/
+/* eslint-enable max-len */
+
 // localhost:3000/api by default, or your personal Heroku server otherwise
+
 router.get('/', (req, res) => {
-  // this is known as a "controller" - the code that replies to your route request
+  /*
+   */
   console.log('You touched the default route!');
   res.json({ message: 'Welcome to the UMD Dining API!' });
 });
