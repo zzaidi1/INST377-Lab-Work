116eeeece7b4569c0984a6753c49fb9afce5a2b4
Alex Leitch <aleitch1@umd.edu>
2022-07-25 10:32:49 -0400

    Fix liveReload server for local development work

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 /* eslint-disable no-console */
+
+/*
+  ## What is this file?
+
+  This file is one of two "apps" that combine to make your dynamic web application.
+  It is your "server" file - the piece of software that responds to requests from the browser
+
+  A request from the browser can be as simple as a URL
+  for example: http://localhost:3000 or https://google.com - both of these perform "GET" requests.
+
+  The requests get passed through routes by this software, which then call "controllers"
+  A "controller" may then incorporate other software, like SQL queries, or requests to other servers
+
+  This file is the "root" of the application and lives in your "root directory"
+  The rest of the files it requires live in other directories.
+*/
+
 // NPM library dependencies
+// These are written by other people, and are included through your 'package.json' file
+// package-lock.json should not be touched - it is a generated file that controls versions.
 import express from 'express';
-import reload from 'livereload';
+import livereload from 'livereload';
+import connectLivereload from 'connect-livereload';
 import dotenv from 'dotenv';
 import path from 'path';
 
 // lab-authored routes
+// Inside this file you'll find all the routes your client can request from this server
 import apiRoutes from './server/routes/apiRoutes.js';
 
 // This sets us up to bring in the .env file, which usually stores application secrets
+// In this class, it stores our application context - 'development'
 dotenv.config();
 
-// this helps a bit with loading files further down
+// this tells our application where we are in your computer
+// so that it can load other files
 const __dirname = path.resolve();
 
 // This is commented out because SQL servers are optional in this class
@@ -20,15 +43,8 @@ const __dirname = path.resolve();
 const app = express(); // Turn on our base application
 const PORT = process.env.PORT || 3000; // Set our port number to be flexible when deployed
 const staticFolder = 'client'; // Our HTML and CSS live in the "staticFolder" - client, public, or build are common names
-let liveReloadServer;
 
-// Add some auto-reloading to our server
-if (process.env.CONTEXT === 'development') {
-  liveReloadServer = reload.createServer();
-  liveReloadServer.watch(path.join(__dirname, staticFolder));
-}
-
-// We let requests come in to our application from any address, which is unusual now
+// We let requests come in to our application from any address, which is unusual post-2018
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -39,6 +55,17 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Add some auto-reloading to our server if we are in a student's development context
+// This will not run if we are on heroku
+let liveReloadServer;
+if (process.env.CONTEXT === 'development') {
+  app.use(connectLivereload());
+
+  liveReloadServer = livereload.createServer();
+  const folder = path.join(__dirname, staticFolder);
+  liveReloadServer.watch(folder);
+}
+
 //  Set our app structure to use 'http://[whatever-you-chose]/ for our main app
 //  And 'http://[whatever-you-chose]/api for our data
 //  While developing, this will be 'http://localhost:3000' - the number set at line 16 in this file.
@@ -48,7 +75,7 @@ app.use('/api', apiRoutes);
 
 // This function declares what happens when our server turns on
 async function bootServer() {
-  try {
+  try { // Try the below, and if something goes wrong ...
     app.listen(PORT, async () => {
       // Turn these back on in later labs
       // const mysql = await db.sequelizeDB;
@@ -56,18 +83,21 @@ async function bootServer() {
       console.log(`Listening on: http//localhost:${PORT}`);
       console.log('environment:', process.env.CONTEXT);
     });
-  } catch (err) {
-    console.error(err);
+  } catch (err) { // Catch and show us the error in our terminal or application logs
+    console.log(err);
   }
 }
 
-// And here, we turn the server on by calling the function
+// Turn the server on by calling the "turn on the server" function
 bootServer();
 
-if (process.env.CONTEXT === 'development') {
-  liveReloadServer.server.once('connection', () => {
-    setTimeout(() => {
-      liveReloadServer.refresh('/');
-    }, 100);
-  });
-}
+// Old options for refreshing live server
+// if (process.env.CONTEXT === 'development') {
+//   console.log('livereload');
+//   liveReloadServer.server.once('connection', () => {
+//     setTimeout(() => {
+//       console.log('liveReload');
+//       liveReloadServer.refresh('/');
+//     }, 100);
+//   });
+// }
