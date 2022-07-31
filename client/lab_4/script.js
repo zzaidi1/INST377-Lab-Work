13002454a5a37e3a3141f24a16a733d8bf7a8fe1
Alex Leitch <aleitch1@umd.edu>
2022-07-31 11:15:48 -0400

    fix bad API name

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -4,7 +4,7 @@ async function mainEvent() { // the async keyword means we can make API requests
   form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
+    const results = await fetch('/api/foodServicePG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
