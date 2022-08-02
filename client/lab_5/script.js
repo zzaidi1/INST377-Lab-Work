86e05da61976876fee996d1295c6428fee7929f6
Alex Leitch <aleitch1@umd.edu>
2022-08-02 11:28:48 -0400

    Carefully annotate week 5

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -4,17 +4,21 @@
 */
 
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
+  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
     const results = await fetch('/api/foodServicePG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+    // It contains all 1,000 records we need to continue with labs next week
   });
 }
 
-// this actually runs first! It's calling the function above
+/*
+  This adds an event listener that fires our main event only once our page elements have loaded
+  The use of the async keyword means we can "await" events before continuing in our scripts
+  In this case, we load some data when the form has submitted
+*/
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
