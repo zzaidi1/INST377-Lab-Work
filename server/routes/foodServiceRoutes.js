e7a4a6f275ffed1a2b7c2d68112edecfbd98cd24
Alex Leitch <aleitch1@umd.edu>
2022-08-02 11:55:52 -0400

    Fix some annotations on foodServicePG to match Lab 5

--- server/routes/foodServiceRoutes.js
+++ server/routes/foodServiceRoutes.js
@@ -2,29 +2,45 @@
 /* eslint-disable no-console */
 import express from 'express';
 
-// this import is in brackets because 'loadFoodServiceData' is set as the name in the export
+/*
+  ## What is this file?
+
+  This is a route handling file for our demo data.
+  It "imports" other modules - like Express, above, or our own work below.
+  Those other files help us process data.
+
+  You should look through them to understand what is happening.
+  The first one is a "middleware" that is asynchronous - it loads data from PG County Open Data and passes it through to our data controllers
+
+  The second one will be used in later labs: it's a way of making data controllers more legible by making them smaller
+*/
+
+// This import is in brackets because 'loadFoodServiceData' is set as the name in the export
 import { loadFoodServiceData } from '../middleware/loadFoodServicesData.js';
 
 // here, basicControllers is a name we made up in this file, which exports an unnamed 'default' object
 import foodServiceControllers from '../controllers/foodServiceControllers.js';
 
-const router = express.Router(); // we are using a new instance of the router per-file
+// we are using a new instance of the router per-file - apiRoutes also has a router instance in it.
+const router = express.Router();
 
 /*
-  ## What is this file?
-  This file contains the main API for class exercises, the Prince George's County Food Safety Data Set.
-*/
-
-/*
-  Here we ask this particular router to use the imported "middleware" to load data
-  _before_ any of our routes load. The order of the code is important in this case.
+  Here we ask this particular router to "use" the imported "middleware" `loadFoodServiceData`
+  to load data _before_ any of our routes load.
+  The order of the code is important in this case.
 */
 router.use(loadFoodServiceData);
 
-/* And here we begin to set up our route methods - GET, POST, PUT and so on */
+/*
+  And here we begin to set up our route methods - GET, POST, PUT and so on
+  For Lab 5, you'll only be working with the `.get()` method
+
+  The other methods come in later!
+*/
+
 router
-  .route('/') // /api/foodServicePG
-  .get(async (req, res) => { // This is your first demo controller!
+  .route('/') // /api/foodServicePG - this address is set in /apiRoutes!
+  .get(async (req, res) => { // Setting this callback to use an async keyword gives us access to await even if we don't use it
     /*
       GET is what happens when you load a route with no other requests
       With your server turned on, you can load this route in your browser at localhost:3000/api/foodServicePG
@@ -32,13 +48,16 @@ router
       Then we will structure our information on the server
       And pass what we need to our client
     */
-    try {
+    try { // Try: This code might work! It might not, though. It's important to res - respond, to seal the request - regardless.
       console.log('You touched the foodService Route!');
-      console.log('req.foodServiceData', req.foodServiceData); // this information comes in through the middleware above
+      console.log('req.foodServiceData', req.foodServiceData.length); // this information comes in through the middleware above
       res.json({ data: req.foodServiceData }); // and here we're closing our route by passing back unfiltered data
-    } catch (err) {
-      // again, these error functions act as a safety to make sure your requests don't hang
-      console.log(err);
+    //
+    //
+    } catch (err) { // Catch: if our above code breaks, this will fire, and send a response to our client saying what happened.
+      console.log(err); // Show the server our error
+
+      // Send a reply to make sure our request from our front-end does not hang open
       res.json({
         message: 'Something went wrong in our foodServicePG GET request',
         error: err
@@ -50,10 +69,10 @@ router
   .delete((req, res) => foodServiceControllers.handleDeleteRequest(req, res));
 
 /*
-  This route is separated out because it is a sub-route of /foodServicePG
+  This route is separated out because it is a 'sub-route' of /foodServicePG
   It turns up if you add an optional string after the main URL - which will be interpreted as "category" of restaurant
   http://localhost:3000/api/foodServicePG/fastfood, for example
-  URLs like this are convenient because they can be bookmarked in the browser
+  URLs like this are convenient because they can be bookmarked in the browser, and still use basic GET methods
 */
 router.route('/:category')
   .get(async (req, res) => {
