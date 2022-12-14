65a1d604c1a447843982133a791690d690cadcea
zzaidi1 <zainzaidi32@gmail.com>
2022-12-14 12:22:39 -0500

    Changes to lab 3 - ZZ

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,28 +7,42 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
-const slides = document.querySelectorAll('.carousel_item');
+slides = document.querySelectorAll('.carousel_item');
 
 // change that "NodeList" into a Javascript "array", to get access to "array methods"
-const slidesArray = Array.from(slides);
+slidesArray = Array.from(slides);
 
 // Figure out how many slides we have available
-const totalSlides = slidesArray.length;
+totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
   // Using the .forEach array method, (array.forEach((element) => { per-element work goes here }))
   // loop through all the slides in your slideArray
   // and remove the 'visible' class from each classList
   // then add a class 'hidden' to all of them
+  slidesArray.forEach((slide) => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  });
+
+  slidesArray[slidePosition].classList.add('visible');
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
 }
 
 function moveToNextSlide() {
+  /* check what current slide is against total slide array.
+  Then get the next image and change the current slide to it.
+  If at the last image and there is no next image, then go to first image */
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else { 
+    slidePosition = slidePosition + 1;
+  }
   /*
     add an if statement here that checks
     if you're already at the max number of slides
@@ -39,6 +53,11 @@ function moveToNextSlide() {
 }
 function moveToPrevSlide() {
   // add your code in here for when you click the "prev" button
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition = slidePosition - 1;
+  }
   /*
     add an if statement here that checks
     if you're already at the first index position for an array
@@ -54,12 +73,12 @@ function moveToPrevSlide() {
 */
 document.querySelector('.next') // Get the appropriate element (<button class="next">)
   .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked next'); // let's tell the client console we made it to this point in the script
+    console.log('clicked next'), // let's tell the client console we made it to this point in the script
     moveToNextSlide(); // call the function above to handle this
   });
 
 document.querySelector('.prev')
-  .addEventListener('click', () => {
-    console.log('clicked prev');
+  .addEventListener('click', () =>  {
+    //console.log('clicked prev');
     moveToPrevSlide();
   });
